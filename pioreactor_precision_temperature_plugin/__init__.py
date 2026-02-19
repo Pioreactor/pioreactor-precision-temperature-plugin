b2c50ef5a8f60d627fc4fbefc0153d6fe58b8c90
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-02-19 10:39:26 -0500

    remove this

--- pioreactor_precision_temperature_plugin/__init__.py
+++ pioreactor_precision_temperature_plugin/__init__.py
@@ -2,13 +2,11 @@
 from pioreactor_precision_temperature_plugin.fir_temperature_automation import Thermostat
 from pioreactor_precision_temperature_plugin.fir_temperature_automation import OnlyRecordTemperature
 from pioreactor_precision_temperature_plugin.fir_temperature_automation import click_temperature_automation
-from pioreactor_precision_temperature_plugin.fir_temperature_automation import click_fir_temperature_bias_trim
 from pioreactor_precision_temperature_plugin.fir_temperature_automation import FIRTemperatureBiasTrimProtocol
 
 __all__ = [
     "Thermostat",
     "OnlyRecordTemperature",
     "click_temperature_automation",
-    "click_fir_temperature_bias_trim",
     "FIRTemperatureBiasTrimProtocol",
 ]
